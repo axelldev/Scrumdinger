ea07f7e48ac66969ce85417b33eca876b3d38fe2
axelldev <axellsolisrubio@gmail.com>
2025-09-07 20:57:30 -0600

    feat: add MeetingHeaderView

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -6,45 +6,37 @@
 //
 
 import SwiftUI
+import ThemeKit
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            
+            VStack {
+                Circle()
+                    .strokeBorder(lineWidth: 24)
 
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
                 }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
-                }
-            }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time remaining")
-            .accessibilityValue("10 minutes")
-
-            Circle()
-                .strokeBorder(lineWidth: 24)
-
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
-                }
-                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
+        .foregroundStyle(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    MeetingView()
+    @Previewable @State var scrum = DailyScrum.sampleData[0]
+    MeetingView(scrum: $scrum)
 }
